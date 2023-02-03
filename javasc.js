fa85f34083b5aae193da0188d8ad77fa46b9eff9
nex-DoT <nex.dot2@gmail.com>
2023-02-03 12:40:54 +0330

    slider auto -v1.5 (adding a bar)

--- javasc.js
+++ javasc.js
@@ -2,6 +2,12 @@ const plus =document.querySelector('#right');
 const perv =document.querySelector('#left');
 const slides = document.querySelectorAll(".slide");
 let time;
+const countEr = document.createElement('div');
+const countEr1 = document.createElement('div');
+const countEr2 = document.createElement('div');
+const bar = document.querySelector('.bar').append(countEr ,countEr1,countEr2);
+const barbalon = document.querySelector('.bar').children;
+console.log(barbalon);
 
 
 function slider(){
@@ -9,33 +15,41 @@ function slider(){
     function next(){
         for(i=0;i<slides.length;i++){
             slides[i].classList.remove('active')
+             barbalon[bm].classList.remove('active')
         }
         bm =( bm + 1 )%slides.length
         slides[bm].classList.add('active')
+        barbalon[bm].classList.add('active')
     }
     plus.addEventListener('click' , function(){
         for(i=0;i<slides.length;i++){
+            barbalon[bm].classList.remove('active')
             slides[i].classList.remove('active')
         }
         bm =( bm + 1 )%slides.length
         slides[bm].classList.add('active')
+        barbalon[bm].classList.add('active')
         clearInterval(time)
         time = setInterval(next, 5000); 
     })
     function pervw(){
         for(i=0;i<slides.length;i++){
             slides[i].classList.remove('active')
+            barbalon[bm].classList.remove('active')
         }
         bm =( bm - 1 + slides.length )%slides.length
+        barbalon[bm].classList.add('active')
         slides[bm].classList.add('active')
     }
     
     perv.addEventListener('click' , function(){
         for(i=0;i<slides.length;i++){
             slides[i].classList.remove('active')
+            barbalon[bm].classList.remove('active')
         }
         bm =( bm - 1 + slides.length )%slides.length
         slides[bm].classList.add('active')
+        barbalon[bm].classList.add('active')
         clearInterval(time)
         time = setInterval(pervw, 5000); 
 
@@ -43,8 +57,5 @@ function slider(){
     
         time = setInterval(next, 5000); 
     
-
-    
-   
 }
 slider()
